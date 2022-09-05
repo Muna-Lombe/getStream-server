9b601be16b5493750597a959b2338a56ec770c10
Muna-Lombe <3223352545@qq.com>
2022-09-05 16:46:08 +0300

    removed child processes folder

--- child-processes/run_server.js
+++
@@ -1,9 +0,0 @@
-const { fork, exec, execFile, spawn } = require("child_process");
-
-const child = fork(__dirname+"/server/index")
-child.on('message', function(message){
-  console.log('Returning /total results');
-  res.setHeader('Content-Type', 'application/json');
-  res.writeHead(200);
-  res.end(message);
-})
